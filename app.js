f3733bcefc1089ac12739c904ffdc17497dbfa31
Isaac Novaes <isaac.novaes@hotmail.com>
2021-10-28 19:31:42 +0200

    Update file. Header functionality and animation are ready

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+function initCounter() {
+	let counter = 0;
+	return function () {
+		return ++counter % 2 === 0 ? false : true;
+	};
+}
+
+const counter = initCounter();
+
+hamburger.addEventListener("click", (event) => {
+	const animateEnter = counter();
+	event.target.classList.toggle("hamburger-active");
+	if (animateEnter) {
+		navLinks.classList.add("nav-links-enter");
+	} else {
+		navLinks.classList.add("nav-links-leave");
+		setTimeout(() => {
+			navLinks.classList.remove("nav-links-leave", "nav-links-enter");
+		}, 1000);
+	}
+});
