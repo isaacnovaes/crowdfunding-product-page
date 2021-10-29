e39c0031fa0f8d4244799d63ad7fcb885c3bd67c
Isaac Novaes <isaac.novaes@hotmail.com>
2021-10-29 23:59:17 +0200

    Update file

--- app.js
+++ app.js
@@ -15,30 +15,29 @@ const counter = initCounter();
 
 // handles the activation of the hamburger links
 hamburger.addEventListener("click", (event) => {
-	const animateEnter = counter();
+	const animateEnter = counter(); // increase counter
 	event.target.classList.toggle("hamburger-active");
-	const bgBlur = document.createElement("div");
+	const bgBlur = document.querySelector(".bg");
 
 	if (animateEnter) {
 		navLinks.classList.add("nav-links-enter");
 		bgBlur.classList.add("bgBlur");
-		header.after(bgBlur);
-		header.style.backgroundColor = "rgba(0, 0, 0, 0.705)";
 	} else {
 		navLinks.classList.add("nav-links-leave");
 		setTimeout(() => {
 			navLinks.classList.remove("nav-links-leave", "nav-links-enter");
-			document.querySelector(".bgBlur").remove();
-			header.style.backgroundColor = "initial";
+			bgBlur.classList.remove("bgBlur");
 		}, 1000);
 	}
 });
 
 // make sure the blur bg and header bg color are reset when coming from
-// a cellphone size screen to a tablet screen size
+// a cellphone size screen to a bigger screen device
 window.addEventListener("resize", () => {
 	if (window.innerWidth >= 600) {
-		document.querySelector(".bgBlur")?.remove();
-		header.style.backgroundColor = "initial";
+		hamburger.classList.remove("hamburger-active");
+		document.querySelector(".bg").classList.remove("bgBlur");
+		navLinks.classList.remove("nav-links-leave", "nav-links-enter");
+		counter();
 	}
 });
