91e123f7b5699bfa73cbf690ef971766f3ae7dad
Isaac Novaes <isaac.novaes@hotmail.com>
2021-10-29 21:45:50 +0200

    Update file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const header = document.querySelector("header");
 
 function initCounter() {
 	let counter = 0;
@@ -12,15 +13,32 @@ function initCounter() {
 
 const counter = initCounter();
 
+// handles the activation of the hamburger links
 hamburger.addEventListener("click", (event) => {
 	const animateEnter = counter();
 	event.target.classList.toggle("hamburger-active");
+	const bgBlur = document.createElement("div");
+
 	if (animateEnter) {
 		navLinks.classList.add("nav-links-enter");
+		bgBlur.classList.add("bgBlur");
+		header.after(bgBlur);
+		header.style.backgroundColor = "rgba(0, 0, 0, 0.705)";
 	} else {
 		navLinks.classList.add("nav-links-leave");
 		setTimeout(() => {
 			navLinks.classList.remove("nav-links-leave", "nav-links-enter");
+			document.querySelector(".bgBlur").remove();
+			header.style.backgroundColor = "initial";
 		}, 1000);
+	}
+});
+
+// make sure the blur bg and header bg color are reset when coming from
+// a cellphone size screen to a tablet screen size
+window.addEventListener("resize", () => {
+	if (window.innerWidth >= 600) {
+		document.querySelector(".bgBlur")?.remove();
+		header.style.backgroundColor = "initial";
 	}
 });
