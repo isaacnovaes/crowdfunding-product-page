2fa2d18295f1d02f469758f7c9a247989fae6248
Isaac Novaes <isaac.novaes@hotmail.com>
2021-11-11 00:10:12 +0100

    Update file. Completed modal is ready

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
-
+// commit changes
+// update units left!
 const hamburger = document.querySelector(".hamburger");
 const buttonBackProject = document.querySelector(".button-block button");
 const navLinks = document.querySelector(".nav-links");
@@ -14,7 +15,9 @@ const modalToggleButtons = document.querySelectorAll(".main-card-modal .toggle-b
 const modalExtraBoxes = document.querySelectorAll(".card-selected");
 const modalContinueButtons = document.querySelectorAll(".card-selected button");
 const errorMessages = document.querySelectorAll(".span");
+const modalCompleted = document.querySelector(".modal-completed");
 const closeModalButton = document.querySelector(".close-modal");
+const closeModalCompleted = document.querySelector(".modal-completed-content button");
 
 let animateLinks = false;
 
@@ -98,11 +101,9 @@ mainModalBlock.addEventListener("mouseout", (event) => {
 });
 
 //////////////////////////////////////////////////////
-// Handles input toggle by clicking on modal card title
-// Fix it! make all card clicklable!!
+// Handles input toggling by clicking on modal card
 mainModalBlock.addEventListener("click", (event) => {
-	if (!event.target.classList.contains("title")) return;
-
+	if (!event.target.closest(".main-card-modal")) return;
 	const cardSelected = event.target.closest(".main-card-modal");
 	const toggleButton = cardSelected.querySelector("input");
 
@@ -112,14 +113,14 @@ mainModalBlock.addEventListener("click", (event) => {
 });
 
 /////////////////////////////////////////////////////
-// Handles input toggle
+// Handles input toggling
 modalForm.addEventListener("input", (event) => {
 	const cardSelected = event.target.closest(".main-card-modal");
 	showExtraBox(cardSelected);
 });
 
 ///////////////////////////////////////////////////
-// Validate input and display success message
+// Validate input and display completed modal
 mainModalBlock.addEventListener("click", (event) => {
 	if (event.target.type !== "button") return;
 
@@ -129,36 +130,43 @@ mainModalBlock.addEventListener("click", (event) => {
 	const errorMessage = continueButton.querySelector("span");
 
 	if (inputValid) {
-		numberInput.value = numberInput.min;
-		// show success modal
-		// select 7 variables
-		// donated, backers, bambooUnits, blackEditionUnits, modalBambooUnits, modalBlackEditionUnits,
+		numberInput.id = "inputValid"; // make it easy to retrieve valid input outside this arrow function
+		resetModal();
+		modalCompleted.classList.add("modal-completed-visible");
 	} else {
 		errorMessage.classList.add("error");
 		setTimeout(() => {
 			errorMessage.classList.remove("error");
-		}, 2000);
+		}, 3000);
 	}
 });
 
 ////////////////////////////////////////////////////
-// Close modal
+// Close default modal
 closeModalButton.addEventListener("click", () => {
-	modalExtraBoxes.forEach((card) => (card.style.display = "none"));
-	errorMessages.forEach((message) => {
-		message.classList.remove("error");
-	});
-	modalToggleButtons.forEach((button) => (button.checked = false));
-	modalBlock.classList.toggle("modal-visible");
-	modalMainCards.forEach((box) => {
-		box.classList.remove("main-card-modal-selected");
-	});
-	setTimeout(() => {
-		bgBlur.classList.toggle("bgBlur-modal-active");
-	}, 500);
+	closeModal();
+});
+
+bgBlur.addEventListener("click", () => {
+	if (modalBlock.classList.contains("modal-visible")) {
+		closeModal();
+	}
 });
 
 ///////////////////////////////////////////////////
+// Close completed modal
+closeModalCompleted.addEventListener("click", () => {
+	resetModalCompleted();
+});
+bgBlur.addEventListener("click", () => {
+	if (modalCompleted.classList.contains("modal-completed-visible")) {
+		resetModalCompleted();
+	}
+});
+
+///////////////////////////////////////////////////
+// Functions
+
 function displayModalAndBgBlur() {
 	bgBlur.style.height = getComputedStyle(document.body).height;
 	modalBlock.classList.toggle("modal-visible");
@@ -177,4 +185,54 @@ function showExtraBox(cardSelected) {
 	if (extraBox) {
 		extraBox.style.display = "flex";
 	}
+}
+
+function closeModal() {
+	resetModal();
+	setTimeout(() => {
+		bgBlur.classList.toggle("bgBlur-modal-active");
+	}, 500);
+}
+
+function resetModal() {
+	modalExtraBoxes.forEach((card) => (card.style.display = "none"));
+	errorMessages.forEach((message) => {
+		message.classList.remove("error");
+	});
+	modalToggleButtons.forEach((button) => (button.checked = false));
+	modalBlock.classList.toggle("modal-visible");
+	modalMainCards.forEach((box) => {
+		box.classList.remove("main-card-modal-selected");
+	});
+}
+
+function resetModalCompleted() {
+	const showNumberUpdate = document.querySelector(".data-card");
+	const moneyDonated = document.querySelector(".data-card .backed h2");
+	const numberOfDonators = document.querySelector(".data-card .backers h2");
+	const progressBar = document.querySelector(".progress-bar-content");
+	const numberInput = document.getElementById("inputValid");
+
+	modalCompleted.classList.remove("modal-completed-visible");
+	bgBlur.classList.toggle("bgBlur-modal-active");
+	showNumberUpdate.scrollIntoView({
+		behavior: "smooth",
+		inline: "center",
+		block: "center",
+	});
+	setTimeout(() => {
+		const updatedMoney = (formatStringToNumber(moneyDonated.innerText, 1) + +numberInput.value).toLocaleString("en-US");
+		moneyDonated.innerText = `$${updatedMoney}`;
+
+		numberOfDonators.innerText = (formatStringToNumber(numberOfDonators.innerText, 0) + 1).toLocaleString("en-US");
+
+		const progressBarNewWidth =
+			(formatStringToNumber(moneyDonated.innerText, 1) + formatStringToNumber(numberInput.value, 1)) / 100000;
+		progressBar.style.width = progressBarNewWidth > 1 ? "100%" : `${progressBarNewWidth * 100}%`;
+	}, 1000);
+	numberInput.id = ""; // reset valid input
+}
+
+function formatStringToNumber(input, slice) {
+	return +input.slice(slice).replace(",", "");
 }
